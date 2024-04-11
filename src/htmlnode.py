c26879ddd9e7dac1c04d7a6c24029b2cd148c85a
JCEOZ <jaroslawzagloba@gmail.com>
2024-04-11 19:56:04 +0200

    added HTMLNode implementation with tests

---
+++ src/htmlnode.py
@@ -0,0 +1,18 @@
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError
+
+    def props_to_html(self):
+        html_attributes = []
+        for prop in self.props:
+            html_attributes.append(f" {prop}=\"{self.props[prop]}\"")
+        return "".join(html_attributes)
+
+    def __repr__(self):
+        return f"HTMLNode(tag={self.tag}, value={self.value}, children={self.children}, props=\"{self.props_to_html()}\")"
---
+++ src/test_htmlnode.py
@@ -0,0 +1,20 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+
+class TestHTMLNode(unittest.TestCase):
+    def test_props_to_html_for_one_prop(self):
+        node = HTMLNode("p", "Example paragraph", None, {"href": "https://www.google.com"})
+        expected = " href=\"https://www.google.com\""
+        self.assertEqual(expected, node.props_to_html())
+
+    def test_props_to_html_for_multiple_props(self):
+        props = {"href": "https://www.google.com", "target": "_blank"}
+        node = HTMLNode("p", "Example paragraph", None, props)
+        expected = " href=\"https://www.google.com\" target=\"_blank\""
+        self.assertEqual(expected, node.props_to_html())
+
+
+if __name__ == '__main__':
+    unittest.main()
