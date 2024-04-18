8d1085de357d6bd6be7c67713b0d2c89c3d837d3
JCEOZ <jaroslawzagloba@gmail.com>
2024-04-18 13:36:05 +0200

    implemented block_to_block_type function

--- src/block.py
+++ src/block.py
@@ -1,3 +1,13 @@
+import itertools
+
+
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_unordered_list = "unordered-list"
+block_type_ordered_list = "ordered-list"
+
 
 def markdown_to_blocks(markdown):
     blocks = []
@@ -13,3 +23,51 @@ def markdown_to_blocks(markdown):
     if len(tmp) > 0:
         blocks.append("\n".join(tmp))
     return blocks
+
+
+def block_to_block_type(block):
+    if __is_block_type_heading__(block): return block_type_heading
+    if __is_block_type_code__(block): return block_type_code
+    if __is_block_type_quote__(block): return block_type_quote
+    if __is_block_type_unordered_list__(block): return block_type_unordered_list
+    if __is_block_type_ordered_list__(block): return block_type_ordered_list
+    return block_type_paragraph
+
+def __is_block_type_heading__(block_type):
+    lines = block_type.split('\n')
+    if len(lines) != 1: return False
+    line = lines[0]
+    for i in range(1, 7):
+        prefix = "".join(itertools.repeat("#", i))
+        if line.startswith(f"{prefix} "):
+            return True
+    return False
+
+
+def __is_block_type_code__(block_type):
+    return block_type.startswith("```") and block_type.endswith("```")
+
+
+def __is_block_type_quote__(block_type):
+    lines = block_type.split('\n')
+    for line in lines:
+        if not line.startswith(">"):
+            return False
+    return True
+
+
+def __is_block_type_unordered_list__(block_type):
+    lines = block_type.split('\n')
+    for line in lines:
+        if not (line.startswith("* ") or line.startswith("- ")):
+            return False
+    return True
+
+
+def __is_block_type_ordered_list__(block_type):
+    lines = block_type.split('\n')
+    for i in range(1, len(lines)):
+        prefix = f"{i}. "
+        if not lines[i - 1].startswith(prefix):
+            return False
+    return True
--- src/test_block.py
+++ src/test_block.py
@@ -1,6 +1,7 @@
 import unittest
 
-from src.block import markdown_to_blocks
+from src.block import markdown_to_blocks, block_type_heading, block_to_block_type, block_type_code, \
+    block_type_paragraph, block_type_quote, block_type_unordered_list, block_type_ordered_list
 
 
 class TestBlock(unittest.TestCase):
@@ -23,6 +24,107 @@ class TestBlock(unittest.TestCase):
 
         self.assertEqual(expected, markdown_to_blocks(markdown))
 
+    def test_block_to_block_type_heading1(self):
+        block = "# heading"
+        expected = block_type_heading
+
+        self.assertEqual(expected, block_to_block_type(block))
+
+    def test_block_to_block_type_heading2(self):
+        block = "## heading"
+        expected = block_type_heading
+
+        self.assertEqual(expected, block_to_block_type(block))
+
+    def test_block_to_block_type_heading3(self):
+        block = "### heading"
+        expected = block_type_heading
+
+        self.assertEqual(expected, block_to_block_type(block))
+
+    def test_block_to_block_type_heading4(self):
+        block = "#### heading"
+        expected = block_type_heading
+
+        self.assertEqual(expected, block_to_block_type(block))
+
+    def test_block_to_block_type_heading5(self):
+        block = "##### heading"
+        expected = block_type_heading
+
+        self.assertEqual(expected, block_to_block_type(block))
+
+    def test_block_to_block_type_heading6(self):
+        block = "###### heading"
+        expected = block_type_heading
+
+        self.assertEqual(expected, block_to_block_type(block))
+
+    def test_block_to_block_type_code(self):
+        block = "```\nsome code example\nwith multiple lines\n ```"
+        expected = block_type_code
+
+        self.assertEqual(expected, block_to_block_type(block))
+
+    def test_block_to_block_type_code_missing_prefix(self):
+        block = "some code example\nwith multiple lines\n ```"
+        expected = block_type_paragraph
+
+        self.assertEqual(expected, block_to_block_type(block))
+
+    def test_block_to_block_type_code_missing_suffix(self):
+        block = "```\nsome code example\nwith multiple lines"
+        expected = block_type_paragraph
+
+        self.assertEqual(expected, block_to_block_type(block))
+
+    def test_block_to_block_type_code_quote(self):
+        block = "> this is some quote\n> which can be life changing\n> if applied"
+        expected = block_type_quote
+
+        self.assertEqual(expected, block_to_block_type(block))
+
+    def test_block_to_block_type_code_quote_invalid_prefix(self):
+        block = "< this is some quote\n< which can be life changing\n< if applied"
+        expected = block_type_paragraph
+
+        self.assertEqual(expected, block_to_block_type(block))
+
+    def test_block_to_block_type_code_quote_missing_prefix(self):
+        block = "> this is some quote\n which can be life changing\n> if applied"
+        expected = block_type_paragraph
+
+        self.assertEqual(expected, block_to_block_type(block))
+
+    def test_block_to_block_type_unordered_list(self):
+        block = "* list item\n- list item\n* list item"
+        expected = block_type_unordered_list
+
+        self.assertEqual(expected, block_to_block_type(block))
+
+    def test_block_to_block_type_unordered_list_missing_prefix(self):
+        block = "* list item\n list item\n* list item"
+        expected = block_type_paragraph
+
+        self.assertEqual(expected, block_to_block_type(block))
+
+    def test_block_to_block_type_ordered_list(self):
+        block = "1. list item\n2. list item\n3. list item"
+        expected = block_type_ordered_list
+
+        self.assertEqual(expected, block_to_block_type(block))
+
+    def test_block_to_block_type_ordered_list_missing_prefix(self):
+        block = "1. list item\n list item\n3. list item"
+        expected = block_type_paragraph
+
+        self.assertEqual(expected, block_to_block_type(block))
+
+    def test_block_to_block_type_ordered_list_invalid_order(self):
+        block = "1. list item\n5. list item\n3. list item"
+        expected = block_type_paragraph
+
+        self.assertEqual(expected, block_to_block_type(block))
 
 if __name__ == '__main__':
     unittest.main()
