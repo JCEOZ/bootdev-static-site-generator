3b4ec23646f7af4c054eb1a6de1b3a1f3d0e2f87
JCEOZ <jaroslawzagloba@gmail.com>
2024-04-18 14:05:33 +0200

    <in progress> implementing markdown_to_html_node function

--- src/block.py
+++ src/block.py
@@ -1,5 +1,6 @@
 import itertools
 
+from src.htmlnode import HTMLNode
 
 block_type_paragraph = "paragraph"
 block_type_heading = "heading"
@@ -71,3 +72,67 @@ def __is_block_type_ordered_list__(block_type):
         if not lines[i - 1].startswith(prefix):
             return False
     return True
+
+# TODO: refactor to use ParentNodes and TextNodes instead of HTMLNode
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    html_nodes = []
+    for block in blocks:
+        block_type = block_to_block_type(block)
+        if block_type == block_type_heading:
+            html_nodes.append(__heading_block_to_html_node__(block))
+        if block_type == block_type_code:
+            html_nodes.append(__code_block_to_html_node__(block))
+        if block_type == block_type_quote:
+            html_nodes.append(__quote_block_to_html_node__(block))
+        if block_type == block_type_unordered_list:
+            html_nodes.append(__unordered_list_block_to_html_node__(block))
+        if block_type == block_type_ordered_list:
+            html_nodes.append(__ordered_list_block_to_html_node__(block))
+        if block_type == block_type_paragraph:
+            html_nodes.append(__paragraph_block_to_html_node__(block))
+    top_level_html_node = HTMLNode(tag="<div>", children=html_nodes)
+    return top_level_html_node
+
+
+def __heading_block_to_html_node__(block):
+    tag = ""
+    if block.startswith("# "): tag = "<h1>"
+    if block.startswith("## "): tag = "<h2>"
+    if block.startswith("### "): tag = "<h3>"
+    if block.startswith("#### "): tag = "<h4>"
+    if block.startswith("##### "): tag = "<h5>"
+    if block.startswith("###### "): tag = "<h6>"
+    return HTMLNode(tag=tag, value=block)
+
+
+def __code_block_to_html_node__(block):
+    child_node = HTMLNode(tag="<code>", value=block)
+    parent_node = HTMLNode(tag="<pre>", children=[child_node])
+    return parent_node
+
+
+def __quote_block_to_html_node__(block):
+    return HTMLNode(tag="<blockquote", value=block)
+
+
+def __unordered_list_block_to_html_node__(block):
+    children_nodes = []
+    lines = block.split('\n')
+    for line in lines:
+        children_nodes.append(HTMLNode(tag="<li>", value=line))
+    parent_node = HTMLNode(tag="<ul>", children=children_nodes)
+    return parent_node
+
+
+def __ordered_list_block_to_html_node__(block):
+    children_nodes = []
+    lines = block.split('\n')
+    for line in lines:
+        children_nodes.append(HTMLNode(tag="<li>", value=line))
+    parent_node = HTMLNode(tag="<ol>", children=children_nodes)
+    return parent_node
+
+
+def __paragraph_block_to_html_node__(block):
+    return HTMLNode(tag="<p>", value=block)
