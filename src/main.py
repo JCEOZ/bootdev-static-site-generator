a00cef6a4a08e281f813a57958d22ca628d53f37
JCEOZ <jaroslawzagloba@gmail.com>
2024-04-19 14:48:05 +0200

    recreating empty /public folder if already exist one

--- src/main.py
+++ src/main.py
@@ -1,9 +1,25 @@
-from textnode import TextNode
+import os
+import shutil
 
+static_folder_path = "../static"
+public_folder_path = "../public"
 
 def main():
-    node = TextNode("This is a TextNode", "bold", "https://www.boot.dev")
-    print(node)
+    static_folder_exists = os.path.exists(static_folder_path)
+    print(f"Static folder exists: {static_folder_exists}")
+
+    if not static_folder_exists:
+        raise Exception("Static folder does not exist")
+
+    public_folder_exists = os.path.exists(public_folder_path)
+    print(f"Public folder exists: {public_folder_exists}")
+
+    if public_folder_exists:
+        print(f"Deleting existing public folder: {public_folder_path}")
+        shutil.rmtree(public_folder_path)
+
+    print(f"Creating new public folder: {public_folder_path}")
+    os.mkdir(public_folder_path)
 
 
 main()
